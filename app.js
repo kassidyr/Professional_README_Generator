a3e36267c271cd4b64fd14de4c2121c48a28a67a
kassidyr <kassidy.ralph@gmail.com>
2021-11-09 10:41:24 -0500

    add prompt

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/readme-template');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/readme-template');
 
-const profileDataArgs = process.argv.slice(9);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'projectTitle',
+            message: 'What is the title of your project?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-console.log(profileDataArgs);
+// const pageMD = generatePage(projectTitle, description, tableOfContents, installation, usage, contributing, tests, emailQuestions);
 
-const [projectTitle, description, tableOfContents, installation, usage, contributing, tests, emailQuestions] = profileDataArgs;
+// fs.writeFile('./README.md', pageMD, err => {
+//     if (err) throw err;
 
-console.log (projectTitle, description, tableOfContents, installation, usage, contributing, tests, emailQuestions);
-
-const pageMD = generatePage(projectTitle, description, tableOfContents, installation, usage, contributing, tests, emailQuestions);
-
-fs.writeFile('./README.md', pageMD, err => {
-    if (err) throw err;
-
-    console.log('README complete! Check out README.md to see the output!');
-});
+//     console.log('README complete! Check out README.md to see the output!');
+// });
